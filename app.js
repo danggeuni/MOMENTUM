865de77014f3b4fb908b4f5ed15ddc8e62333c67
danggeuni <102232204+danggeuni@users.noreply.github.com>
2022-06-21 22:48:45 +0900

    my loginform update

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const loginForm = document.querySelector(".login-form");
-const loginInput = document.querySelector(".login-form input");
-const greeting = document.querySelector(".greeting");
+const loginInput = document.querySelector(".login-input");
+const hello = document.querySelector(".hello");
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "userName";
+const USERNAME_TEXT = "userName";
+const HIDDEN_TEXT = "hidden";
 
-function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const userName = loginInput.value;
-  console.log(userName);
-
-  localStorage.setItem(USERNAME_KEY, userName);
-
-  paintGreetings(userName);
+function loginAfter(userName) {
+  loginForm.classList.add(HIDDEN_TEXT); // form 숨기고
+  hello.classList.remove(HIDDEN_TEXT); // span 표시
+  hello.innerText = `Hello ${userName}`; // 값으로 인사
 }
 
-function paintGreetings(userName) {
-  greeting.innerText = `Hello ${userName}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+function handleSubmit(event) {
+  event.preventDefault(); // 새로고침 방지
+  const userName = loginInput.value; // input 값 저장
+  loginAfter(userName); // form 숨기고, span 표시, input 값으로 인사
+  localStorage.setItem(USERNAME_TEXT, userName); // input 값 localStorage에 저장
 }
 
-const savedUserName = localStorage.getItem(USERNAME_KEY);
+const loginUser = localStorage.getItem(USERNAME_TEXT); // localStorage 값 불러오기
 
-if (savedUserName === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+if (loginUser === null) {
+  // 만약 localstorage에 데이터가 없다면,
+  loginForm.addEventListener("submit", handleSubmit); // input값 local에 저장하는 펑션 실행
 } else {
-  paintGreetings(savedUserName);
+  // 데이터가 있으면,
+  loginAfter(loginUser); // form 숨기고 span 표시, local 값으로 인사
 }
